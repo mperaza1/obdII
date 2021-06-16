d2e71fa1adac9d8195fa9867e5dff8f8eda49a5b
MPeraza23 <36338258+MPeraza23@users.noreply.github.com>
2021-06-16 15:48:38 -0400

    Add debug

--- test.py
+++ test.py
@@ -3,16 +3,27 @@ import obd
 def main():
   print('Hello World')
 
-  ports = obd.scan_serial()      # return list of valid USB or RF ports
+  '''ports = obd.scan_serial()      # return list of valid USB or RF ports
   print(ports)                    # ['/dev/ttyUSB0', '/dev/ttyUSB1']
   connection = obd.OBD()
   print(connection.status())
 
-  cmd = obd.commands.RPM
+  while(1):
+      cmd = obd.commands.RPM
+      response = connection.query(cmd)
+      print(response.value)'''
 
-  response = connection.query(cmd)
+  obd.logger.setLevel(obd.logging.DEBUG)
 
-  print(response.value)
+  connection = obd.OBD() # auto-connects to USB or RF port
+  print("Return from obd.OBD")
+  cmd = obd.commands.SPEED # select an OBD command (sensor)
+
+  response = connection.query(cmd) # send the command, and parse the response
+
+  print(response.value) # returns unit-bearing values thanks to Pint
+  print(response.value.to("mph")) # user-friendly unit conversions
+
 
 if (__name__ == "__main__"):
   main()
